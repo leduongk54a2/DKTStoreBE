26caca03ebcbdc4950246441a52c5c4573bc13e2
Le Manh Duong (VTI.D5) <duong.lemanh@vti.com.vn>
2023-02-20 14:59:08 +0700

    feat(study laravel): study route mvc laravel

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    //
+    public function  view(): string
+    {
+        return 'Day la mot bai viet';
+    }
+}
